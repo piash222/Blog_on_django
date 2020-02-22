54ad059f049ddd5f5ba88846ad897b6af7836746
Shihab Uddin Piash <piash222@gmail.com>
2020-02-22 21:30:26 +0600

    blog post creation and reverse to detail

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.PostListView.as_view(), name="blog-home"),
     path("post/<int:pk>", views.PostDetailView.as_view(), name="post-detail"),
+    path("post/new/", views.PostCreateView.as_view(), name="post-create"),
     path("about/", views.about, name="blog-about")
 ]
 
