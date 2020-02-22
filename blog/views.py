844b75e040cf611f65487a7c33d05cec4b459e31
Shihab Uddin Piash <piash222@gmail.com>
2020-02-22 11:04:53 +0600

    blog home generic list view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 
 
 # Create your views here.
@@ -9,6 +10,13 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  # default: <app_name>/<model_name>_<view_name>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
 
 def about(request):
