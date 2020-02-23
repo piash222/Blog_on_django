eca91b91c316ec1db2ed1ecd633e1c615474c0dc
Shihab Uddin Piash <piash222@gmail.com>
2020-02-23 23:19:10 +0600

    post update

--- users/views.py
+++ users/views.py
@@ -6,17 +6,20 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def register(request):
-    if request.method == "POST":
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get("username")
-            messages.success(request, f"Account created for {username}")
-            return redirect('login')
-
+    if request.user.is_authenticated:
+        return redirect('blog-home')
     else:
-        form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+        if request.method == "POST":
+            form = UserRegisterForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get("username")
+                messages.success(request, f"Account created for {username}")
+                return redirect('login')
+
+        else:
+            form = UserRegisterForm()
+        return render(request, 'users/register.html', {'form': form})
 
 
 @login_required
