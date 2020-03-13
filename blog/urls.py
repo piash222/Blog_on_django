e3527b69036c11c4dd80dfde46d3bacb27314a04
Md Shihab Uddin <piash222@gmail.com>
2020-03-14 01:06:54 +0600

    page for user posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.PostListView.as_view(), name="blog-home"),
+    path("user/<str:username>/", views.UserPostListView.as_view(), name="user-post"),
     path("post/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
     path("post/<int:pk>/update/", views.PostUpdateView.as_view(), name="post-update"),
     path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post-delete"),
